06be28048e2ac92d2108281b29bd1f95f382f889
AhmedAboAbadi <ahmed.m.aboabadi@gmail.com>
2022-09-12 12:05:32 +0300

    kanban drop and drag

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content:['./pages/*.{js},./components/*.{js}'],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
